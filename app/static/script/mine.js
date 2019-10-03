2b60232a3ebca802c27371444c6460e2b818b8ca
cc <1321607222@qq.com>
2019-10-03 23:13:44 +0800

    我的页面中滚动出现另一个搜素框

--- app/static/script/mine.js
+++ app/static/script/mine.js
@@ -1,8 +1,24 @@
 $(function(){
-    new IScroll('.mine-main',{
+    var myScroll =  new IScroll('.mine-main',{
         click:true,
-        tap:true
+        tap:true,
+        probeType:3
     });
+
+    myScroll.on('scroll',function(){
+        console.log(1);
+        if(myScroll.y<-50){
+            $('.mine-header').addClass('ani');
+            $('.mine-header').css('display','flex');
+           
+            $('.mine-main-header').css('display','none');
+        }else{
+            $('.mine-header').css('display','none');
+            $('.mine-main-header').css('display','flex');
+        }
+            
+    })
+
 
 
 })
