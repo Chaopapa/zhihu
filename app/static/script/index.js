318578220f16894e94c1c447cc22f5417b87b0dc
cc <1321607222@qq.com>
2019-10-05 15:40:26 +0800

    完成推荐页面基本样式

--- app/static/script/index.js
+++ app/static/script/index.js
@@ -3,13 +3,10 @@ $(function(){
     new MyScroll('.index-main',{
         click:true,
         tap:true
+    });
 
-        
-
-
-
-
-
+    $('.index-nav-ad').click(function(){
+        $('.i-main').load('./recommend.html')
     })
 
 
