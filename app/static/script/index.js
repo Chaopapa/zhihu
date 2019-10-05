8d5e7ab182bdfacfca6655b40cdd019eb8bc162b
cc <1321607222@qq.com>
2019-10-05 16:08:48 +0800

    scroll

--- app/static/script/index.js
+++ app/static/script/index.js
@@ -1,15 +1,18 @@
-$(function(){
+$(function() {
+  //关注
+  new MyScroll(".index-main", {
+    click: true,
+    tap: true
+  });
 
-    new MyScroll('.index-main',{
-        click:true,
-        tap:true
-    });
-
-    $('.index-nav-ad').click(function(){
-        $('.i-main').load('./recommend.html')
-    })
-
-
-
-
-})
+  $(".index-nav-ad").click(function() {
+    $(".i-main").load("./recommend.html");
+    setInterval(function(){
+        //推荐
+    new MyScroll(".rec-main", {
+        click: true,
+        tap: true
+      });
+    },100);
+  });
+});
